9662ae9e3365bd650c663cf6ecd61d5f9ed7b383
jagarzone6 <jagarzone@gmail.com>
2019-08-28 22:28:02 -0500

    fixed use of expected conditions

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,10 +1,11 @@
 class HomePage {
   constructor(EC) {
-    this.ADD_USER_BUTTON = By.css("table button[type='addw']");
+    this.ADD_USER_BUTTON_SELECTOR = "table button[type='add']";
+    this.ADD_USER_BUTTON = By.css(this.ADD_USER_BUTTON_SELECTOR);
     this.EC = EC;
   }
   async clickOnAddUser() {
-    await browser.wait(browser.findElement(this.ADD_USER_BUTTON), 5000);
+    await browser.wait(this.EC.visibilityOf($(this.ADD_USER_BUTTON_SELECTOR)), 5000);
     return browser.findElement(this.ADD_USER_BUTTON).click();
   }
 }
