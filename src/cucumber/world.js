d3930742392d608037738f9781d0ba6f78210b71
jagarzone6 <jagarzone@gmail.com>
2019-08-28 22:16:12 -0500

    updated world to use expected conditions

--- src/cucumber/world.js
+++ src/cucumber/world.js
@@ -1,13 +1,17 @@
 var { setWorldConstructor } = require("cucumber");
 var { setDefaultTimeout } = require("cucumber");
-setDefaultTimeout(25 * 1000);
 var config = require('./../../resources/config.json')[process.env.ENV || 'default'];
+var HomePage = require("./../pages/home-page.js");
+var EC = protractor.ExpectedConditions;
+
 
 class World {
   constructor() {
     this.testData = {};
     this.appURL = config.appURL;
+    this.homePage = new HomePage(EC);
   }
 }
 
 setWorldConstructor(World);
+setDefaultTimeout(25 * 1000);
