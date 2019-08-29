f36ce7f3a8ef260156fbc46d1cd6cfb48bd5b087
jagarzone6 <jagarzone@gmail.com>
2019-08-29 00:36:01 -0500

    added assertions config

--- src/cucumber/world.js
+++ src/cucumber/world.js
@@ -3,8 +3,9 @@ var { setDefaultTimeout } = require("cucumber");
 var config = require('./../../resources/config.json')[process.env.ENV || 'default'];
 var HomePage = require("./../pages/home-page.js");
 var CreateUserModal = require("./../pages/modals/create-user-modal.js");
-
 var EC = protractor.ExpectedConditions;
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
 
 
 class World {
@@ -14,8 +15,10 @@ class World {
     this.homePage = new HomePage(EC);
     this.createUserModal = new CreateUserModal(EC);
     this.newUser = config.newUser;
+    this.assert = chai.assert;
   }
 }
 
+chai.use(chaiAsPromised);
 setWorldConstructor(World);
 setDefaultTimeout(25 * 1000);
